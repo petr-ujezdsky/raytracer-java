8766670ed8e1c4b3fae8c06bc607d6b5599abfb5
petr-ujezdsky <petr.ujezdsky@gmail.com>
2021-03-21 19:31:43 +0100

    Initial commit

---
+++ src/main/java/org/pujezdsky/raytracer/App.java
@@ -0,0 +1,10 @@
+package org.pujezdsky.raytracer;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
